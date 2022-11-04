3efc270a18160b1c6e9a5d9e43fa6a6d11e2f215
Alissa Cohan <alissacohan18@gmail.com>
2022-11-04 19:58:38 -0400

    added prompt for user to select password criteria

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,26 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
+function generatePassword(){
+  var lCase = window.confirm ("If a lowercase letter is needed, click OK to apply and continue, or Cancel to move on")
+  var uCase = window.confirm ("If a uppercase letter is needed, Click OK to apply and continue, or Cancel to move on")
+  var num = window.confirm ("If a number is needed, click OK to apply and continue, or Cancel to move on")
+  var sChar = window.confirm ("If a special character is needed, click OK to apply and continue, or Cancel to move on")
+   var pLength = prompt("Choose password length (between 8-128 characters)")
+   if (pLength > 7 && pLength < 129) {
+    console.log("validated")
+   }
+   else {pLength = prompt("Choose password length (type a number between 8-128 characters)")}
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// prompt for password criteria when button is clicked
-
-// booleon for: lwr/upr case, numeric, and special char 
-
-// #char from 8-128
 
 // validation??? Then my input should be v and at least 1 char type should be selected"
 
