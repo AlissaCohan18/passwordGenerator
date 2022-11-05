9d307fdffb88dc56c1ad35f31979214ed61c4f9a
Alissa Cohan <alissacohan18@gmail.com>
2022-11-04 20:30:43 -0400

    added while loop to ensure one character type was selected

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,18 @@ function writePassword() {
 }
 
 function generatePassword(){
-  var lCase = window.confirm ("If a lowercase letter is needed, click OK to apply and continue, or Cancel to move on")
-  var uCase = window.confirm ("If a uppercase letter is needed, Click OK to apply and continue, or Cancel to move on")
-  var num = window.confirm ("If a number is needed, click OK to apply and continue, or Cancel to move on")
-  var sChar = window.confirm ("If a special character is needed, click OK to apply and continue, or Cancel to move on")
+  var secure = false
+  while (secure === false) {
+    var lCase = window.confirm ("If a lowercase letter is needed, click OK to apply and continue, or Cancel to move on")
+    var uCase = window.confirm ("If a uppercase letter is needed, Click OK to apply and continue, or Cancel to move on")
+    var num = window.confirm ("If a number is needed, click OK to apply and continue, or Cancel to move on")
+    var sChar = window.confirm ("If a special character is needed, click OK to apply and continue, or Cancel to move on")
+    if (lCase === false && uCase === false && num === false && sChar === false){ 
+       window.alert("***!At least One Character Type must be selected for a more secure password!***")
+    } else {
+       secure = true
+      }
+    }
   var ver = "not yet ;)"  
   while (ver != "validated") {
     var pLength = prompt("Choose password length (between 8-128 characters)")
@@ -22,12 +30,12 @@ function generatePassword(){
     }
 }
 
+// Math.floor(Math.random() * 101);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-// validation??? Then my input should be v and at least 1 char type should be selected"
 
 // generate password based on criteria
 
